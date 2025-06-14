cfb5552ec544dfe3d2d53162804f019a1fd0d09b
wchesedh <wchesedh@gmail.com>
2025-06-15 01:13:06 +0800

    fix: font config, remove mjs, and update build settings for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  basePath: '',
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig 
