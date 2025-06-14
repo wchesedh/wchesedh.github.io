35bd871b27017abff23280c885cf12ab8572a130
wchesedh <wchesedh@gmail.com>
2025-06-14 22:53:30 +0800

    Update: fix styles path

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/trader-portfolio' : '',
+  basePath: '',
 }
 
 module.exports = nextConfig 
